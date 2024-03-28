85a8b8410ec275283f201584d60b23b1707e7f88
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-03-28 13:08:34 -0400

    Yoooo its working most the time! But for some reason I am getting undefined every now and then! So close!

--- script.js
+++ script.js
@@ -7,19 +7,18 @@ console.log(add2(2));
 // Tried a lot of things at this point. Do I need a param? Do I need to assign Rock / Paper / Scissors to a value?
 
 function getComputerChoice() {
-    let Rock = 0
-    let Paper = 1
-    let Scissors = 2
-    if (getComputerChoice() === 0){
+    Math.floor(Math.random() * 3);
+    
+    if (Math.floor(Math.random() *3) === 0){
         return 'Rock';
-    }
-    if (getComputerChoice() === 1){
+    } else {};
+    if (Math.floor(Math.random() *3) === 1){
         return 'Paper';
-    }
-    if (getComputerChoice() === 0){
+    } else {};
+    if (Math.floor(Math.random() *3) === 2){
         return 'Scissors';
-    }
-    return Math.floor(Math.random() * 3 );
+    } else{};
+   
 
 }
 
