c3cbf727eb2b30ae1f36891c1db669bf097de04e
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-03-28 11:55:22 -0400

    Not functioning yet. Working on getComputerChoice

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-console.log("Hello World!")
 
 function add2(num) {
     return num + 2;
 }
 
 console.log(add2(2));
+// Tried a lot of things at this point. Do I need a param? Do I need to assign Rock / Paper / Scissors to a value?
+
+function getComputerChoice() {
+    let Rock = 1
+    let Paper = 2
+    let Scissors = 3
+    Math.floor(Math.random() + 3)
+}
+
+console.log(getComputerChoice())
