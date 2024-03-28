337b7d369ebfc5b79870413624fdea69d1d3fdc0
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-03-28 12:45:16 -0400

    Close? Added if statements, but now I am seeing error that says "RangeError: Maximum call stack size exceeded"

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ console.log(add2(2));
 // Tried a lot of things at this point. Do I need a param? Do I need to assign Rock / Paper / Scissors to a value?
 
 function getComputerChoice() {
-    let Rock = 0;
-    let Paper = 1;
-    let Scissors = 2;
+    let Rock = 0
+    let Paper = 1
+    let Scissors = 2
+    if (getComputerChoice() === 0){
+        return 'Rock';
+    }
+    if (getComputerChoice() === 1){
+        return 'Paper';
+    }
+    if (getComputerChoice() === 0){
+        return 'Scissors';
+    }
     return Math.floor(Math.random() * 3 );
 
 }
