37a98ff2b77c83072747cde7a827415af5d5178a
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-03-28 13:28:52 -0400

    Back to start of Math.random() * 3

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ console.log(add2(2));
 // Tried a lot of things at this point. Do I need a param? Do I need to assign Rock / Paper / Scissors to a value?
 
 function getComputerChoice() {
-    Math.floor(Math.random() * 2);
+    Math.floor(Math.random() * 3);
     
-    if (Math.floor(Math.random() * 2) === 0){
+    if (Math.floor(Math.random() * 3) === 0){
         return 'Rock';
     };
-    if (Math.floor(Math.random() * 2) === 1){
+    if (Math.floor(Math.random() * 3) === 1){
         return 'Paper';
     };
-    if (Math.floor(Math.random() * 2) === 2){
+    if (Math.floor(Math.random() * 3) === 2){
         return 'Scissors';
     };
 }
 
-console.log(getComputerChoice(1))
+console.log(getComputerChoice())
