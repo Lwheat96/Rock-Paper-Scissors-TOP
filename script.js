2596af496969f7ee85a07106adcbb536a7cd41da
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-03-28 12:37:46 -0400

    I tried let rock = 0, it did not work...

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ console.log(add2(2));
 // Tried a lot of things at this point. Do I need a param? Do I need to assign Rock / Paper / Scissors to a value?
 
 function getComputerChoice() {
+    let Rock = 0;
+    let Paper = 1;
+    let Scissors = 2;
     return Math.floor(Math.random() * 3 );
+
 }
 
 console.log(getComputerChoice())
