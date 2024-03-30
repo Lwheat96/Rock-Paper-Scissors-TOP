d3729b1ceb7a0a1bf01334bf225bb3dd617d7455
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-03-30 16:22:57 -0400

    Getting an undefined message currently... Ugh! Want to get this right.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-function add2(num) {
-    return num + 2;
-}
-
-console.log(add2(2));
 // Tried a lot of things at this point. Do I need a param? Do I need to assign Rock / Paper / Scissors to a value?
 // Thank you guys this part is working now!!! 
 // Made them all if statements!
@@ -20,19 +14,49 @@ function getComputerChoice() {
     }
 }
 
+function playerChoice() {
+    parseInt(prompt("Choose rock, paper, or scissors.").toLowerCase);
+    return 'rock' | 'paper' | 'scissors';
+}
 // Starting the playRound() here. I didn't need a pop up? I used parseInt but that did not seem to be the direction I was supposed to go.
 // This is showing correctly in console.log(playRound)
+/* 
+It is correct but it isn't logically determining a winner. How do I set the params
+for each iteration? 
+Rock beats scissors, loses to paper, draws rock. 
+Paper loses to scissors, ties paper. 
+Scissors ties scissors.
+*/
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'rock') {
-        return 'You tied!'
+    if ( playerSelection === 'rock' && computerSelection === 'rock'){
+        return "Draw!";
     }
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You lose!'
+    if ( playerSelection === 'paper' && computerSelection === 'rock'){
+        return "Loser!";
     }
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You Win!'
+    if ( playerSelection === 'scissors' && computerSelection === 'rock'){
+        return "Win!";
     }
+    if ( playerSelection === 'rock' && computerSelection === 'paper'){
+        return "Loser!";
+    }
+    if ( playerSelection === 'rock' && computerSelection === 'scissors'){
+        return "Win!";
+    }
+    if ( playerSelection === 'paper' && computerSelection === 'paper'){
+        return "Draw!";
+    }
+    if ( playerSelection === 'paper' && computerSelection === 'scissor'){
+        return "Loser!";
+    }
+    if ( playerSelection === 'scissors' && computerSelection === 'paper'){
+        return "Win!";
+    }
+    if ( playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return "Draw!";
+    }
+    
 }
 
 /* function playGame(playRound) {
@@ -41,7 +65,8 @@ function playRound(playerSelection, computerSelection) {
 
 // I feel like these should go to the top of my code?
 
-// const playerSelection = parseInt(prompt("Enter rock, paper, or scissors").toLowerCase);
+const playerSelection = playerChoice(); 
+//Commented this out to get rid of pop up everytime...
 const computerSelection = getComputerChoice();
 
-// console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
